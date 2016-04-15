6322a550d7b6e53f0746850ecfd65b57203a476d
Michael Arbogast <marboga@gmail.com>
2016-04-14 21:01:18 -0700

    functionality restored

--- getting_started/server.js
+++ getting_started/server.js
@@ -1,3 +1,4 @@
+// require express so that we can build an express app
 var express = require('express');
 // require path so that we can use path stuff like path.join
 var path = require('path');
@@ -9,5 +10,5 @@ app.use(bodyParser.json());
 // set up a static file server that points to the "client" directory
 app.use(express.static(path.join(__dirname, './client')));
 app.listen(8000, function() {
-  console.log('listening over here: 8000');
+  console.log('cool stuff on: 8000');
 });
