6b87411b3951f7b0806aeb74b58619f5167b4ab5
Michael Arbogast <marboga@gmail.com>
2016-04-14 17:14:02 -0700

    basic functionality - server up

---
+++ getting_started/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+// require path so that we can use path stuff like path.join
+var path = require('path');
+// instantiate the app
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// set up a static file server that points to the "client" directory
+app.use(express.static(path.join(__dirname, './client')));
+app.listen(8000, function() {
+  console.log('listening over here: 8000');
+});
